39cc71106e768285cee7598c44ce455f4a8b5032
neemiasbsilva <neemiasbsilva@gmail.com>
2019-10-30 09:26:47 -0400

    Create my second model call Transaction and Update DB

--- account/models.py
+++ account/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
     dt_creation = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Categorys'
 
 class Transactions(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField()
     description = models.CharField(max_length=200)
     value = models.DecimalField(max_digits=7, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    notes = models.TextField()
+    notes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Transactions'

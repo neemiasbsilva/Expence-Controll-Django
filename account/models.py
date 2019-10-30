d934638f28aef5f6a78c3cc69cf30e8297d89fcc
neemiasbsilva <neemiasbsilva@gmail.com>
2019-10-30 09:18:00 -0400

    Update migration add Transactions Model

--- account/models.py
+++ account/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     dt_creation = models.DateTimeField(auto_now_add=True)
+
+class Transactions(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=200)
+    value = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    notes = models.TextField()
